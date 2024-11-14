77075beb6a642f07da5b93e6a296c40012824c1c
tbazarov <159284610+tbazarov@users.noreply.github.com>
2024-11-14 19:13:46 +0700

    Update main.c
    Добавлена сортировка для строк

--- main.c
+++ main.c
@@ -2,18 +2,25 @@
 #include <stdlib.h>
 #include "ssortdeclars.h"
 
+#define MAX_STRINGS 100
+#define MAX_LENGTH 100
+
 
 
 int main() 
 {
     int n;
+    char *strings[MAX_STRINGS];
     printf("Введите количество элементов: ");
     scanf("%d", &n);
+    getchar();
 
     printf("Выберите тип данных:\n");
     printf("1. int\n");
     printf("2. float\n");
     printf("3. char\n");
+    printf("4. string\n");
+
     int type;
     scanf("%d", &type);
 
@@ -47,6 +54,20 @@ int main()
             scanf(" %c", &((char*)arr)[i]);
         }
         selection_sort(arr, n, sizeof(char), compare_char);
+    }
+
+    else if (type == 4)
+    {
+
+        printf("Введите строки: \n");
+        for (int i = 0; i < n; i++) 
+        {
+            strings[i] = (char *)malloc(MAX_LENGTH * sizeof(char));
+            fgets(strings[i], MAX_LENGTH, stdin);
+
+        }
+        string_sel_sort(strings, n);
+
     } 
 
     printf("Отсортированный массив:\n");
@@ -74,6 +95,17 @@ int main()
             printf("%c ", ((char*)arr)[i]);
         }
     }
+
+    else if (type == 4)
+    {
+       printf("Отсортированные строки: \n");
+        for (int i = 0; i < n; i++) 
+        {
+            printf("%s", strings[i]);
+            free(strings[i]); 
+        }
+    }
+
     printf("\n");
 
     free(arr);
